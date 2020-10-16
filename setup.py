74030e9ae00384141c1b855e91268a2c63a3525d
Andrey Vokin <andrey.vokin@jetbrains.com>
2020-10-16 10:46:36 +0200

    Release 1.0.6

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from distutils.core import setup
 setup(
     name='jetbrains-issues-dataset',
     packages=['jetbrains_issues_dataset', 'jetbrains_issues_dataset.idea'],
-    version='1.0.5',
+    version='1.0.6',
     license='MIT',
     description='Dataset of JetBrains issues',
     long_description='Datasets of IDEA issues. See https://github.com/avokin2/jetbrains-issues-dataset',
     author='Andrey Vokin',
     author_email='andrey.vokin@gmail.com',
     url='https://github.com/avokin2/jetbrains-issues-dataset',
-    download_url='https://github.com/avokin2/jetbrains-issues-dataset/archive/v_1.0.5.tar.gz',
+    download_url='https://github.com/avokin2/jetbrains-issues-dataset/archive/v_1.0.6.tar.gz',
     keywords=[],
     install_requires=[],
     classifiers=[
